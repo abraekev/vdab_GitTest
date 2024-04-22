3927aa36cfb2104cd2bc62a090d01a8ea2d8244b
abraekev <arnout.braekevelt@gmail.com>
2024-04-22 15:23:11 +0200

    cc

--- GitConsole/Program.cs
+++ GitConsole/Program.cs
@@ -1,4 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Sup, Gitm 2nd");
-Console.WriteLine("BONJOUR");
+Console.WriteLine("master branch");
