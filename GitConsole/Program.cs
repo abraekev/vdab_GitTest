1adeeb2d574486e5aa76f09cf9b12b3b7e9f1e4a
abraekev <arnout.braekevelt@gmail.com>
2024-04-22 15:20:57 +0200

    bjnjour

--- GitConsole/Program.cs
+++ GitConsole/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Sup, Gitm 2nd");
-Console.WriteLine("Sup, Gitm 2nd");
+Console.WriteLine("BONJOUR");
